a70b548bd8afe71030e2dec7671e0f9d294682fe
Glenn Roberts <guraaku@streborg.com>
2026-09-02 16:03:57 +1000

    initial commit basic property

---
+++ specification.ts
@@ -0,0 +1,9 @@
+import { extract, always } from "@antithesishq/bombadil";
+
+export * from "@antithesishq/bombadil/browser/defaults";
+
+const pageTitle = extract(state => state.document.title || "");
+
+export const pageTitleNeverContainsThe = always(() =>
+  !pageTitle.current.toLowerCase().includes("the")
+);
