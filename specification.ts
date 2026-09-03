784b8ca6d0d3dba291bad88615db9b7cc11cda9a
guraaku <36474222+guraaku@users.noreply.github.com>
2026-09-03 10:48:59 +1000

    Rename pageTitleNeverContainsThe to pageTitleNeverContainsAnd

--- specification.ts
+++ specification.ts
@@ -4,6 +4,6 @@ export * from "@antithesishq/bombadil/browser/defaults";
 
 const pageTitle = extract(state => state.document.title || "");
 
-export const pageTitleNeverContainsThe = always(() =>
+export const pageTitleNeverContainsAnd = always(() =>
   !pageTitle.current.toLowerCase().includes("and")
 );
